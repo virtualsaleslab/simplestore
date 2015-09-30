69dae48139e06dfb7b2786b3e28c3c898281ec8c
Tom Janssens <Tom@corebvba.be>
2015-09-30 20:49:32 +0200

    Reformatting

--- src/Domain/Authorization.hs
+++ src/Domain/Authorization.hs
@@ -1,16 +1,26 @@
 module Domain.Authorization where
 
-import Domain.Models(TenantId,ProjectId)
-import Domain.Authentication(IdentityId)
+import           Domain.Authentication (IdentityId)
+import           Domain.Models         (ProjectId, TenantId)
 
-data TenantSpec = AllTenants | SpecificTenant TenantId deriving (Eq,Show,Read)
-data TenantRole = TenantAdmin deriving (Eq,Show,Read)
+data TenantSpec = AllTenants
+                | SpecificTenant TenantId
+                deriving (Eq,Show,Read)
 
-data ProjectSpec = AllProjects | AllTenantProjects TenantId | OwnTenantProjects TenantId | SpecificProject ProjectId deriving (Eq,Show,Read)
-data ProjectRole = ProjectEditor | ProjectReviewer deriving (Eq,Show,Read)
+data TenantRole = TenantAdmin
+                  deriving (Eq,Show,Read)
+
+data ProjectSpec = AllProjects
+                 | AllTenantProjects TenantId
+                 | OwnTenantProjects TenantId
+                 | SpecificProject ProjectId
+                 deriving (Eq,Show,Read)
+
+data ProjectRole = ProjectEditor
+                 | ProjectReviewer deriving (Eq,Show,Read)
 
 data Claim = TenantClaim TenantRole TenantSpec
-          | ProjectClaim  ProjectRole ProjectSpec
-          | IdentityClaim IdentityId
-          | SuperAdmin
-          deriving (Eq,Show,Read)
+           | ProjectClaim  ProjectRole ProjectSpec
+           | IdentityClaim IdentityId
+           | SuperAdmin
+           deriving (Eq,Show,Read)
