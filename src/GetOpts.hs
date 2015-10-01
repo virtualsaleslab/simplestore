dbdd630884876e652c419c2f9b0c5d8babfe9235
Tom Janssens <tom@corebvba.be>
2015-10-01 11:25:15 +0200

    Command line support!

---
+++ src/GetOpts.hs
@@ -0,0 +1,53 @@
+module GetOpts  where
+
+import Options.Applicative
+
+data Command = ResetDatabase ResetDatabaseOptions
+             | Server ServerOptions
+
+data ServerOptions = ServerOptions
+    { port :: Int
+    }
+
+serverOptions :: Parser Command
+serverOptions = Server . ServerOptions
+      <$>  option auto
+            ( long "port"
+            <> short 'p'
+            <> value 8081
+            <> metavar "PORT"
+            <> help "Port to run the server on"
+            ) 
+
+data ResetDatabaseOptions = ResetDatabaseOptions
+    { force :: Bool
+    }
+
+resetDatabaseOptions :: Parser Command
+resetDatabaseOptions = ResetDatabase . ResetDatabaseOptions
+    <$> switch
+       (  long "force"
+       <> short 'f'
+       <> help  "Force execution of the reset"
+       )
+
+data Options = Options
+   { optGlobalDBName :: String
+   , optCommand :: Command
+   }
+
+options :: Parser Options
+options = Options
+    <$> strOption
+       (   long "dbname"
+        <> short 'd'
+        <> value "test.db"
+        <> help "The filename of the Sqlite database to use"
+        <> metavar "DBNAME"
+       )
+    <*> subparser (
+           command "resetdatabase" (info resetDatabaseOptions
+             (progDesc "Reset the server. Use --force to execute"))
+        <> command "server" (info serverOptions
+             (progDesc "Start a webserver"))
+        )
--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,32 @@ import Network.Wai.Handler.Warp (run)
 import Servant                  (serve)
 import Servers.MainServer
 
-app :: Application
-app = serve mainAPI mainServer
+import GetOpts
+import Options.Applicative
+import Lib.ServantHelpers(liftIO)
+
+import DB.Admin(buildDatabase)
+
+runServer :: ServerOptions -> Options -> IO ()
+runServer opt gOpt = do
+    let p = port opt
+    putStrLn $ "Running webserver on port " ++ show p
+    run p $ serve mainAPI mainServer
+
+runResetDB :: ResetDatabaseOptions -> Options -> IO ()
+runResetDB opts gOpts = if force opts
+    then buildDatabase >>= putStrLn
+    else putStrLn "Use the --force option if you want this to work"
+
+runCommand :: Options -> IO ()
+runCommand opts = case optCommand opts of
+  Server        x -> runServer x opts
+  ResetDatabase x -> runResetDB x opts
 
 main :: IO ()
-main = run 8081 app
+main = execParser opts >>= runCommand
+  where opts = info ( helper <*> options )
+                (  header "SimpleStorage by arealities.com"
+                <> progDesc "A simple multi-tenant database accessible over HTTP. Run with -h to view available commands\n"
+                <> fullDesc
+                )
